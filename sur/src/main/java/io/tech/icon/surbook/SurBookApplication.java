54e8d9f77632e9c11285a1af66eac727f7903ec1
Jules Nsenda <jules.nsenda@gmail.com>
2022-04-18 12:21:39 +0200

    Start learning from Spring boot up and running book

---
+++ sur/src/main/java/io/tech/icon/surbook/SurBookApplication.java
@@ -0,0 +1,13 @@
+package io.tech.icon.surbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurBookApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SurBookApplication.class, args);
+    }
+}
---
+++ sur/src/test/java/io/tech/icon/surbook/SurBookApplicationTests.java
@@ -0,0 +1,14 @@
+package io.tech.icon.surbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurBookApplicationTests
+{
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
