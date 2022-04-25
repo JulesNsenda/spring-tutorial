744a0c5e01b51fedd566c14d55cbe917050a95d0
Jules Nsenda <jules.nsenda@gmail.com>
2022-04-25 12:00:30 +0200

    Adding files and structure of the app

---
+++ sur-aircraft/sur-aircraft/src/main/java/io/tech/icon/suraircraft/SurAircraftApplication.java
@@ -0,0 +1,13 @@
+package io.tech.icon.suraircraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurAircraftApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SurAircraftApplication.class, args);
+    }
+}
---
+++ sur-aircraft/sur-aircraft/src/test/java/io/tech/icon/suraircraft/SurAircraftApplicationTests.java
@@ -0,0 +1,15 @@
+package io.tech.icon.suraircraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurAircraftApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
